48404c63708dcc8cab720011e5a6ae8a449dab24
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-05-16 19:32:00 -0500

    updating to api mint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <meta property="og:image" content={`https://7f02867cd22acd60803070fbbcac9bc7.ipfscdn.io/ipfs/bafybeidyzzuf72qjuge7zrbjvupve6kl4surrsbayi6a2k7jqzo3hvgow4/GM.gif`} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content={`https://7f02867cd22acd60803070fbbcac9bc7.ipfscdn.io/ipfs/bafybeidyzzuf72qjuge7zrbjvupve6kl4surrsbayi6a2k7jqzo3hvgow4/GM.gif`} />
-        <meta property="fc:frame:post_url" content={`https://main--gmgnminting.netlify.app/mint`} />
+        <meta property="fc:frame:post_url" content={`https://main--gmgnminting.netlify.app/api/mint`} />
         <meta property="fc:frame:button:1" content="Get started" />
       </Head>
       <body>
